21fbe34d2b7fb50fa2e34556a687cfc88234242c
WeiYing Lee <weiyinglee@cpp.edu>
2016-01-26 16:55:59 -0800

    Testing leaflet

--- public/javascript/leaflet/map.js
+++ public/javascript/leaflet/map.js
@@ -17,8 +17,12 @@ $(function(){
 */
 
 window.onload = function() {
-  var map = L.map('map').setView([43.07.265, -89.400929], 10);
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map); 
+   var map = L.map('map').setView([-41.2858, 174.78682], 14);
+        mapLink = 
+            '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+        L.tileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; ' + mapLink,
+            maxZoom: 18,
+            }).addTo(map);
 }
