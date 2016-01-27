ff5aa1a10b73c2247cfd5df7bb5ec224e09a0ad6
WeiYing Lee <weiyinglee@cpp.edu>
2016-01-26 16:33:39 -0800

    Trying to test out simple map for leaflet

--- public/javascript/leaflet/map.js
+++ public/javascript/leaflet/map.js
@@ -1,5 +1,24 @@
-//Declare Map Object
+/*
+$(function(){
 
-// Reference the tiles
+ //Declare Map Object
+  var map = L.map('map').setView([51.505, -0.09], 13);
 
-// Markers and Popups
+  // Reference the tiles
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'your.mapbox.project.id',
+    accessToken: 'your.mapbox.public.access.token'
+  }).addTo(map);
+
+  // Markers and Popups
+});
+*/
+
+window.onload = function() {
+  var map = L.map('map').setView([43.07.265, -89.400929], 10);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map); 
+}
