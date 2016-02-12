aa9732ca42b5e90cf3407b4755796714554c6d66
WeiYing Lee <weiyinglee@cpp.edu>
2016-02-11 20:55:40 -0800

    Set up part of login logic

--- public/modules/user/controllers/LoginCtrl.js
+++ public/modules/user/controllers/LoginCtrl.js
@@ -1,6 +1,24 @@
 'use strict';
 
-var LoginController = App.controller('LoginCtrl', [ '$scope', 
+var LoginController = App.controller('LoginCtrl', [ '$scope', '$http',
 	function($scope, $http){
+	  $scope.userName = "";
+	  $scope.userPassword = "";
+	  
+	  //Authentication for login
+	  $scope.auth = function(){
+	  	
+	  	//authentication backend API
+	  	var inputData = {
+	  		name: userName,
+	  		password: userPassword
+	  	};
+
+	  	$http.post('', inputData).then(function(response){
+	  		//Do something
+	  	}, function(error){
+	  		console.error(error);
+	  	});
+	  };
 
   	}]);
